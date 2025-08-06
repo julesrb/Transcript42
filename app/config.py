b67b55668856b8bca0212c47304e77420fc68323
Jules <jbjulesbernard3@gmail.com>
2025-08-06 23:49:05 +0200

    updated redirection url with domain name

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 UID = os.getenv("42UID")
 SECRET = os.getenv("42SECRET")
-REDIRECT_URI = os.getenv("OAUTH_REDIRECT_URI", "http://68.183.66.13/oauth_redirect")
+REDIRECT_URI = os.getenv("OAUTH_REDIRECT_URI", "http://transcript42.project-cloud.cloud/oauth_redirect")
 AUTH_URL = "https://api.intra.42.fr/oauth/authorize"
 TOKEN_URL = "https://api.intra.42.fr/oauth/token"
 USER_URL = "https://api.intra.42.fr/v2/me"
